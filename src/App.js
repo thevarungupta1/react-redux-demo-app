44fed14240712e2403d159f83a8fd73ca54fadf5
Varun Gupta <thevarungupt1@gmail.com>
2022-03-14 13:48:36 +0530

    add product details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import ProductListing from "./components/ProductListing";
+import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom';
+import ProductDetail from "./components/ProductDetail";
 
 function App() {
   return (
     <div className="container">
-       <ProductListing />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<ProductListing />} />
+          <Route exact path="/product/:productId" element={<ProductDetail />} />
+          {/* <Route>404 Not Found</Route> */}
+        </Routes>
+      </Router>
+       
     </div>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Product = (props) => {
-    const {productName, image} = props.data;
-    return(
-        <div className="col-sm-3">
-          <div className="card">
-          <img src={ 'http://rjtmobile.com/grocery/images/' + image } class="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{ productName }</h5>
-              
-              <a href="#" className="btn btn-primary">
-               Add to cart
-              </a>
-            </div>
+  const { _id, productName, image } = props.data;
+  return (
+    <div className="col-sm-3">
+      <Link to={`/product/${_id}`}>
+        <div className="card">
+          <img
+            src={"http://rjtmobile.com/grocery/images/" + image}
+            class="card-img-top"            
+          />
+          <div className="card-body">
+            <h5 className="card-title">{productName}</h5>
+
+            <a href="#" className="btn btn-primary btn-block">
+              Add to cart
+            </a>
           </div>
         </div>
-    )
-}
+      </Link>
+    </div>
+  );
+};
 export default Product;
